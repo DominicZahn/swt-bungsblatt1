8cb2f7a803d8a77106e327ee46781916575a6ef2
Dominic Zahn <zahndominic@web.de.com>
2019-05-05 18:04:54 +0200

    added junit and created RotateTest.java with setUp method

---
+++ iMage/jmjrst.main/src/test/java/main/org/jis/RotateTest.java
@@ -0,0 +1,15 @@
+package main.org.jis;
+
+import org.jis.generator.Generator;
+import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;;
+
+public class RotateTest {
+  private Generator generator;
+
+  public void setUp() {
+    generator = new Generator(null, 0);
+  }
+}
