0ece59b3d2037affd39b7390f14563f5008318e1
Dominic Zahn <zahndominic@web.com>
2019-06-05 00:44:05 +0200

    created funtinality of getPlugins in PluginManagment

--- iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
+++ iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
@@ -1,29 +1,44 @@
 package org.iMage.plugins;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ServiceLoader;
+
 /**
- * Knows all available plug-ins and is responsible for using the service loader API to detect them.
+ * Knows all available plug-ins and is responsible for using the service loader
+ * API to detect them.
  *
  * @author Dominik Fuchss
  */
 public final class PluginManagement {
 
-  /**
-   * No constructor for utility class.
-   */
-  private PluginManagement() {
-    throw new IllegalAccessError();
-  }
+	/**
+	 * No constructor for utility class.
+	 */
+	private PluginManagement() {
+		throw new IllegalAccessError();
+	}
 
-  /**
-   * Return an {@link Iterable} Object with all available {@link PluginForJmjrst PluginForJmjrsts}
-   * sorted according to the length of their class names (shortest first).
-   *
-   * @return an {@link Iterable} Object containing all available plug-ins alphabetically sorted
-   * according to their class names.
-   */
-  public static Iterable<PluginForJmjrst> getPlugins() {
-    //TODO: implement me!
-    return null;
-  }
-
+	/**
+	 * Return an {@link Iterable} Object with all available {@link PluginForJmjrst
+	 * PluginForJmjrsts} sorted according to the length of their class names
+	 * (shortest first).
+	 *
+	 * @return an {@link Iterable} Object containing all available plug-ins
+	 *         alphabetically sorted according to their class names.
+	 */
+	public static Iterable<PluginForJmjrst> getPlugins() {
+		ArrayList<PluginForJmjrst> pluginList = new ArrayList<PluginForJmjrst>();
+		ServiceLoader<PluginForJmjrst> serviceLoader = ServiceLoader.load(PluginForJmjrst.class);
+		for (PluginForJmjrst plugin : serviceLoader) {
+			pluginList.add(plugin);
+		}
+		pluginList.sort(new Comparator<PluginForJmjrst>() {
+			@Override
+			public int compare(PluginForJmjrst pfj1, PluginForJmjrst pfj2) {
+				return pfj1.compareTo(pfj2);
+			}
+		});
+		return pluginList;
+	}
 }
