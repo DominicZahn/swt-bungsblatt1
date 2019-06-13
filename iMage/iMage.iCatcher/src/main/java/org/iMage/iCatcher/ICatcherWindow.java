9e0e2b57c466233be47bc257afb4496e53f499dd
Dominic Zahn <zahndominic@web.com>
2019-06-14 00:39:28 +0200

    changed the slidebar to a real scroll bar and fiexed some issues

--- iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/ICatcherWindow.java
+++ iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/ICatcherWindow.java
@@ -3,18 +3,22 @@ package org.iMage.iCatcher;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JSlider;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JScrollBar;
 
 public class ICatcherWindow extends JFrame {
 	private GridLayout globalLayout = new GridLayout(2, 6);
 	private FlowLayout topButtons = new FlowLayout();
-	private JButton showCurve = new JButton();
-	private JButton saveCurve = new JButton();
-	private JButton saveHDR = new JButton();
+	private JButton showCurve = new JButton("SHOW CURVE");
+	private JButton saveCurve = new JButton("SAVE CURVE");
+	private JButton saveHDR = new JButton("SAVE HDR");
+	private JScrollBar scrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
 	
 	public ICatcherWindow() {
 		super("iCatcher");
@@ -23,17 +27,37 @@ public class ICatcherWindow extends JFrame {
 		add(new Label("original"),0);
 		//preview for the HDR-picture
 		add(new Label("preview"), 1);
-		//slider
-		JSlider slider = new JSlider();
-		slider.setMajorTickSpacing(10);
-		slider.setMaximum(100);
-		slider.setMinimum(0);
-		add(slider, 2);
+		//scroll bar
+		add(scrollBar, 2);
 		//buttons
 		JPanel buttons = new JPanel(topButtons);
-		showCurve.setText("SHOW CURVE");
-		saveCurve.setText("SAVE CURVE");
-		saveHDR.setText("SAVE HDR");
+		buttons.add(showCurve);
+		buttons.add(saveCurve);
+		buttons.add(saveHDR);
+		showCurve.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//show curve
+			}
+		});
+		saveCurve.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//save curve
+				
+			}
+		});
+		saveHDR.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//save HDR
+				
+			}
+		});
 		add(buttons, 3);
+		
 	}
 }
