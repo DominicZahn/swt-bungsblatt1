65c8eb717c6fabd89cf52a53990aa8fbfd24068b
Dominic Zahn <zahndominic@web.com>
2019-06-13 22:31:00 +0200

    added all relevant classes

---
+++ iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/ImageHDRWindow.java
@@ -0,0 +1,5 @@
+package org.iMage.iCatcher;
+
+public class ImageHDRWindow {
+
+}
--- iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/iCatcher.java
+++
@@ -1,7 +0,0 @@
-package org.iMage.iCatcher;
-
-public class iCatcher {
-	public static void main(String[] args) {
-		
-	}
-}
