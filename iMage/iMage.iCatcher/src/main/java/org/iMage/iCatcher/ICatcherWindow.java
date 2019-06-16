8f5b869a37d9d1bcd7f474413e33900bd807635f
Dominic Zahn <zahndominic@web.com>
2019-06-16 16:56:17 +0200

    add functioanlity for the lambda text field

--- iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/ICatcherWindow.java
+++ iMage/iMage.iCatcher/src/main/java/org/iMage/iCatcher/ICatcherWindow.java
@@ -2,6 +2,7 @@ package org.iMage.iCatcher;
 
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -22,7 +23,7 @@ import javax.swing.JTextField;
 public class ICatcherWindow extends JFrame {
 	private final int LEFTSIDEX = 30;
 	private final int RIGHTSIDEX = 410;
-	//just so there is something
+	// just so there is something
 	private Label originalSlideShow = new Label("original");
 	//
 	private JButton buttonPreview = new JButton("preview");
@@ -42,102 +43,131 @@ public class ICatcherWindow extends JFrame {
 	private JButton buttonLoadCurve = new JButton("LOAD CURVE");
 	private JButton buttonRunHDrize = new JButton("RUN HDrize");
 
+	private final double LAMBDADEFAULT = 500;
+	// global variables
+	private double lambda = LAMBDADEFAULT;
+
 	public ICatcherWindow() {
 		super("iCatcher");
-		setLayout(null);
+		setRawStructure();
+		setActionListeners();
+	}
+
+	private void setActionListeners() {
+		//textFieldLambda
+		textFieldLabmda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String input = textFieldLabmda.getText();
+				try {
+					lambda = Double.parseDouble(input);
+					textFieldLabmda.setForeground(Color.BLACK);
+					textFieldLabmda.setText(Double.toString(lambda));
+				} catch (NumberFormatException ne) {
+					textFieldLabmda.setForeground(Color.RED);
+				}
+			}
+		});
 		
-		//original pictures
+		//
+
+	}
+
+	private void setRawStructure() {
+		setLayout(null);
+
+		// original pictures
 		originalSlideShow.setLocation(LEFTSIDEX, 30);
 		originalSlideShow.setSize(350, 250);
 		add(originalSlideShow);
-		
-		//preview picture / button
+
+		// preview picture / button
 		buttonPreview.setLocation(RIGHTSIDEX, 30);
 		buttonPreview.setSize(350, 250);
 		add(buttonPreview);
-		
-		//slider for the original pictures
+
+		// scroll bar for the original pictures
 		scrollBarOriginal.setMinimum(0);
-		scrollBarOriginal.setMaximum(1050);//3 *  width of the picture
+		scrollBarOriginal.setMaximum(1050);// 3 * width of the picture
 		scrollBarOriginal.setLocation(LEFTSIDEX, 290);
 		scrollBarOriginal.setSize(350, 20);
 		add(scrollBarOriginal);
-		
-		final int GAP = 5; //gap between the three following buttons
-		//button SHOW CURVE
+
+		final int GAP = 5; // gap between the three following buttons
+		// button SHOW CURVE
 		buttonShowCurve.setLocation(RIGHTSIDEX, 290);
 		buttonShowCurve.setSize(120, 20);
 		add(buttonShowCurve);
-		
-		//button SAVE CURVE
+
+		// button SAVE CURVE
 		buttonSaveCurve.setLocation(RIGHTSIDEX + 120 + GAP, 290);
 		buttonSaveCurve.setSize(120, 20);
 		add(buttonSaveCurve);
-		
-		//button SAVE HDR
+
+		// button SAVE HDR
 		buttonSaveHDR.setLocation(RIGHTSIDEX + (120 + GAP) * 2, 290);
 		buttonSaveHDR.setSize(100, 20);
 		add(buttonSaveHDR);
-		
-		//label drop-down Camera Curve
+
+		// label drop-down Camera Curve
 		labelCameraCurve.setLocation(LEFTSIDEX, 330);
 		labelCameraCurve.setSize(350, 20);
 		add(labelCameraCurve);
-		
-		//drop-down Camera Curve
+
+		// drop-down Camera Curve
 		comboBoxCameraCurve.setLocation(LEFTSIDEX, 350);
 		comboBoxCameraCurve.setSize(350, 20);
 		add(comboBoxCameraCurve);
-		
-		//label drop-down Tone Mapping 
+
+		// label drop-down Tone Mapping
 		labelToneMapping.setLocation(LEFTSIDEX, 390);
 		labelToneMapping.setSize(350, 20);
 		add(labelToneMapping);
-		
-		//drop-down Tone Mapping
+
+		// drop-down Tone Mapping
 		comboBoxToneMapping.setLocation(LEFTSIDEX, 410);
 		comboBoxToneMapping.setSize(350, 20);
 		add(comboBoxToneMapping);
-		
+
 		final int SLIDERWIDTH = 250;
-		//label which displays the current value of the slider
+		// label which displays the current value of the slider
 		labelSamplesValue.setAlignment(Label.CENTER);
 		labelSamplesValue.setLocation(RIGHTSIDEX, 330);
 		labelSamplesValue.setSize(SLIDERWIDTH, 20);
 		add(labelSamplesValue);
-		
-		//samples slider
+
+		// samples slider
 		sliderSamples.setLocation(RIGHTSIDEX, 350);
 		sliderSamples.setSize(SLIDERWIDTH, 20);
 		sliderSamples.setMinimum(0);
 		sliderSamples.setMaximum(1000);
 		sliderSamples.setValue(500);
 		add(sliderSamples);
-		
+
 		final int POSXLAMBDA = RIGHTSIDEX + SLIDERWIDTH + 30;
 		final int WIDTHLAMBDA = 350 - (SLIDERWIDTH + 30);
-		//label lambda
+		// label lambda
 		labelLambda.setAlignment(Label.RIGHT);
 		labelLambda.setLocation(POSXLAMBDA, 330);
 		labelLambda.setSize(WIDTHLAMBDA, 20);
 		add(labelLambda);
-		
-		//text field lambda
+
+		// text field lambda
 		textFieldLabmda.setLocation(POSXLAMBDA, 350);
 		textFieldLabmda.setSize(WIDTHLAMBDA, 20);
 		textFieldLabmda.setHorizontalAlignment(JTextField.RIGHT);
 		add(textFieldLabmda);
-		
-		//button LOAD DIR
+
+		// button LOAD DIR
 		buttonLoadDir.setLocation(RIGHTSIDEX, 410);
 		buttonLoadDir.setSize(100, 20);
 		add(buttonLoadDir);
-		//button LOAD CURVE
+		
+		// button LOAD CURVE
 		buttonLoadCurve.setLocation(RIGHTSIDEX + 105, 410);
 		buttonLoadCurve.setSize(120, 20);
 		add(buttonLoadCurve);
-		
-		//button RUN HDrize
+
+		// button RUN HDrize
 		buttonRunHDrize.setLocation(RIGHTSIDEX + 230, 410);
 		buttonRunHDrize.setSize(120, 20);
 		add(buttonRunHDrize);
